9e2f27239e7c243a7f5746b596161d8c3a0c331f
Warley Coutinho <warleycoutinho@icloud.com>
2021-10-12 21:28:53 -0300

    atualizando o index.js

--- index.js
+++ index.js
@@ -6,27 +6,31 @@ const db = require("./config/db");
 app.db = db;
 
 consign()
-    // .then(cors())
-    .include("./config/passport.js")
-    .then("./config/middlewares.js")
-    .then("./api/validation.js")
-    .then("./api")
-    // .then("./schedule"
-    .then("./config/routes.js")
-    .into(app);
+  // .then(cors())
+  .include("./config/passport.js")
+  .then("./config/middlewares.js")
+  .then("./api/validation.js")
+  .then("./api")
+  // .then("./schedule"
+  .then("./config/routes.js")
+  .into(app);
 
-// const port = normalizaPort(process.env.PORT || "3000");
-// function normalizaPort(val) {
-//     const port = parseInt(val, 10);
-//     if (isNaN(port)) {
-//         return val;
-//     }
-//     if (port >= 0) {
-//         return port;
-//     }
-//     return false;
-// }
-app.listen(process.env.PORT || "3000");
-// app.listen(port, () => {
-//     console.log(`Projeto Crass Final-Backend.Sendo Executando, na porta ${port}.`);
-// });
+const port = normalizaPort(process.env.PORT || "3000");
+function normalizaPort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+app.listen(port, () => {
+  console.log(
+    `Projeto Crass Final-Backend.Sendo Executando, na porta ${port}.`
+  );
+
+  // app.listen(process.env.PORT || "3000");
+});
